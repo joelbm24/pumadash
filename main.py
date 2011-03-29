406b79238377c6299ca89b04374ae2b8824e62a7
Joel <joelbm24@gmail.com>
2011-03-29 14:07:34 -0700

    added more views

--- main.py
+++ main.py
@@ -1,19 +1,35 @@
 import sys, os
 from flask import Flask, render_template
 
-sys.path.append(os.environ["HOME"] + "/puma/lib")
-import puma
-p = puma.Puma()
+error=False
+
+try:
+    sys.path.append(os.environ["HOME"] + "/puma/lib")
+    import puma
+    p = puma.Puma()
+except:
+    error=True
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template("index.html")
+    if not error:
+        return render_template("index.html")
+    else:
+        return "<h1>Puma isn't installed!</h1>"
 
-@app.route('/instances/')
-def instances():
-    return render_template('instances.html', p=p)
+@app.route('/info/')
+@app.route('/info/<opt>')
+def info(opt=None):
+    if opt == None:
+        # add template
+        return "info page"
+
+    try:
+        return render_template(opt+'.html', p=p)
+    except:
+        return "<h1>404: Page not found</h1>"
 
 if __name__ == '__main__':
     app.run()
